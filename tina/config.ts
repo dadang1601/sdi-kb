724d3d041283f060e9abab2ab22c8ac90bbb6e25
dadang1601 <dadang.suryana1601@gmail.com>
2025-12-24 17:57:08 +0000

    initial commit docusaurus + tina

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main",
+
+  clientId: process.env.TINA_CLIENT_ID!,
+  token: process.env.TINA_TOKEN!,
+
+  build: {
+    publicFolder: "static",
+    outputFolder: "admin",
+  },
+
+  media: {
+    tina: {
+      mediaRoot: "img",
+      publicFolder: "static",
+    },
+  },
+
+  schema: {
+    collections: [
+      {
+        name: "docs",
+        label: "Documentation",
+        path: "docs",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Content",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
